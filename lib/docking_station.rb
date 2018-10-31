4fb8369bd325881e47e2b5fc89f654a86c287390
kiedunne <kie.dunne@gmail.com>
2018-10-31 15:45:43 +0000

    Challenge 10-14 passed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,8 +1,20 @@
 require_relative './bike.rb'
 
 class DockingStation
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
-    Bike.new
+    fail "No docked bikes." if @bikes.empty?
+    @bikes.pop
+  end
+
+  def dock(bike)
+    fail "No room for bike." if @bikes.count >= 20
+    @bikes << bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,13 +9,36 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  it "Releases bike" do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
-end
+  # it "Releases bike" do
+  #   bike = subject.release_bike
+  #   expect(bike).to be_working
+  # end
 
-#   it "Docking Station return a Bike instance" do
-#     expect(subject).to release_bike.should eq(Bike.new)
-#   end
-# end
+   it "Releases a working bike" do
+     expect(Bike.new).to respond_to :working?
+   end
+
+   it "Releases the docked bike" do
+     bike = Bike.new
+     subject.dock(bike)
+     expect(subject.release_bike).to eq bike
+   end
+
+   it { is_expected.to respond_to(:dock).with(1).argument }
+
+   it "Seeing docked bike" do
+     bike = Bike.new
+     subject.dock(bike)
+     expect(subject.bikes).to eq [bike] 
+   end
+
+   it "No bikes released if empty" do
+     expect {subject.release_bike}.to raise_error "No docked bikes."
+   end
+
+   it "Cannot dock bike if station full" do
+     20.times {subject.dock(Bike.new)}
+     expect {subject.dock(Bike.new)}.to raise_error "No room for bike."
+   end
+
+end
